4cc6ab809b8c2b7f139ca2c97844b626ed34858a
trungnguyens93 <trungnguyens93@gmail.com>
2016-12-28 08:04:19 +0700

    Add menu

---
+++ app/src/main/java/com/framgia/moviedb/ui/fragments/MainFragment.java
@@ -0,0 +1,23 @@
+package com.framgia.moviedb.ui.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.framgia.moviedb.R;
+
+/**
+ * Created by trungnguyens93gmail.com on 12/27/16.
+ */
+public class MainFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        return view;
+    }
+}
